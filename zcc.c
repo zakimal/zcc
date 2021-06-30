dbd6e807ab319909bdc069af8f6ac35021e79622
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-06-30 12:58:43 +0000

    add/sub compiler

--- zcc.c
+++ zcc.c
@@ -5,14 +5,37 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        fprintf(stderr, "引数の個数が正しくありません\n");
+        fprintf(stderr, "error: 2 arguments should be given.\n");
         return 1;
     }
+
+    char *p = argv[1];
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-')
+        {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "error: found unexpected charactor: '%c'\n", *p);
+    }
+
     printf("  ret\n");
+
     return 0;
 }

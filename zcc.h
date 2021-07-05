29123ea042adfbddb9987fe360777937ec958d28
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-07-05 06:44:48 +0000

    add block

--- zcc.h
+++ zcc.h
@@ -73,6 +73,7 @@ typedef enum
     ND_LE,        // <=
     ND_ASSIGN,    // =
     ND_RETURN,    // "return"
+    ND_BLOCK,     // { ... }
     ND_EXPR_STMT, // Expression statement
     ND_VAR,       // Variable
     ND_NUM,       // Integer
@@ -86,6 +87,7 @@ struct Node
     Node *next;    // Next node
     Node *lhs;     // Left-hand side
     Node *rhs;     // Right-hand side
+    Node *body;    // Block
     Obj *var;      // Used if kind == ND_VAR
     int val;       // Used if kind == ND_NUM
 };

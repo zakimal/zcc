9e291be1cb81e4741904165949e97174676f08e4
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-09-15 09:52:34 +0000

    decay an array to a pointer in a func param context

--- test/function.c
+++ test/function.c
@@ -80,6 +80,11 @@ static int static_fn()
     return 3;
 }
 
+int param_decay(int x[])
+{
+    return x[0];
+}
+
 int main()
 {
     ASSERT(3, ret3());
@@ -112,6 +117,13 @@ int main()
     ASSERT(1, bool_fn_sub(0));
 
     ASSERT(3, static_fn());
+
+    ASSERT(3, (
+                  {
+                      int x[2];
+                      x[0] = 3;
+                      param_decay(x); // passed as *int
+                  }));
 
     return 0;
 }

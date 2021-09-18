61a7b2c57e8ac193719361f0ae1b12da292195e6
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-09-18 09:41:27 +0000

    resolve conflict between labels and typedefs

--- test/control.c
+++ test/control.c
@@ -171,5 +171,13 @@ int main()
                       i;
                   }));
 
+    ASSERT(1, (
+                  {
+                      typedef int foo;
+                      goto foo;
+                  foo:;
+                      1;
+                  }));
+
     return 0;
 }

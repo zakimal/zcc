49fdcf786b3cfa299aed1c9ad3e1215ce39ed949
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-09-30 13:47:30 +0000

    support initialization a struct with other struct

--- test/initialize.c
+++ test/initialize.c
@@ -300,6 +300,16 @@ int main()
                       z = y = x;
                       z.b;
                   }));
+    ASSERT(1, (
+                  {
+                      typedef struct
+                      {
+                          int a, b;
+                      } T;
+                      T x = {1, 2};
+                      T y = x;
+                      y.a;
+                  }));
 
     return 0;
 }
